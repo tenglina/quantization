6d2461a7db3970843f545bde66bf9c5f1b6ebeb6
tenglina <tenglina@msu.edu>
2024-06-03 12:25:03 -0400

    add VGG model class

---
+++ model.py
@@ -0,0 +1,53 @@
+from collections import OrderedDict, defaultdict
+
+from matplotlib import pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
+from tqdm.auto import tqdm
+
+import torch
+from torch import nn
+
+assert torch.cuda.is_available(), \
+"The current runtime does not have CUDA support." 
+
+
+class VGG(nn.Module):
+  ARCH = [64, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M']
+
+  def __init__(self) -> None:
+    super().__init__()
+
+    layers = []
+    counts = defaultdict(int)
+
+    def add(name: str, layer: nn.Module) -> None:
+      layers.append((f"{name}{counts[name]}", layer))
+      counts[name] += 1
+
+    in_channels = 3
+    for x in self.ARCH:
+      if x != 'M':
+        # conv-bn-relu
+        add("conv", nn.Conv2d(in_channels, x, 3, padding=1, bias=False))
+        add("bn", nn.BatchNorm2d(x))
+        add("relu", nn.ReLU(True))
+        in_channels = x
+      else:
+        # maxpool
+        add("pool", nn.MaxPool2d(2))
+    add("avgpool", nn.AvgPool2d(2))
+    self.backbone = nn.Sequential(OrderedDict(layers))
+    self.classifier = nn.Linear(512, 10)
+
+  def forward(self, x: torch.Tensor) -> torch.Tensor:
+    # backbone: [N, 3, 32, 32] => [N, 512, 2, 2]
+    x = self.backbone(x)
+
+    # avgpool: [N, 512, 2, 2] => [N, 512]
+    # x = x.mean([2, 3])
+    x = x.view(x.shape[0], -1)
+
+    # classifier: [N, 512] => [N, 10]
+    x = self.classifier(x)
+    return x
