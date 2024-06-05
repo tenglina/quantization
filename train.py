028e9096f8e0a373c8b1fad32f2854b0ab7feb75
tenglina <tenglina@msu.edu>
2024-06-05 19:23:56 -0400

    add training functionality

---
+++ train.py
@@ -0,0 +1,38 @@
+import model  
+
+
+
+def train(
+  model: nn.Module,
+  dataloader: DataLoader,
+  criterion: nn.Module,
+  optimizer: Optimizer,
+  scheduler: LambdaLR,
+  callbacks = None
+) -> None:
+  model.train()
+
+  for inputs, targets in tqdm(dataloader, desc='train', leave=False):
+    # Move the data from CPU to GPU
+    inputs = inputs.cuda()
+    targets = targets.cuda()
+
+    # Reset the gradients (from the last iteration)
+    optimizer.zero_grad()
+
+    # Forward inference
+    outputs = model(inputs)
+    loss = criterion(outputs, targets)
+
+    # Backward propagation
+    loss.backward()
+
+    # Update optimizer and LR scheduler
+    optimizer.step()
+    scheduler.step()
+
+    if callbacks is not None:
+        for callback in callbacks:
+            callback()
+
+            
