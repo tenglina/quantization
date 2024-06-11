27765622a07102864a1ce9278f37c8d1109e7657
tenglina <tenglina@msu.edu>
2024-06-10 22:20:29 -0400

    function: perform kmeans quantization on given float tensor

--- k_means_quantization.py
+++ k_means_quantization.py
@@ -8,8 +8,18 @@
 
 from collections import namedtuple
 from fast_pytorch_kmeans import KMeans
+import torch 
 
 LookupTable = namedtuple('LookupTable', ['centroids, labels'])
 
 def k_means_quantize(fp32_tensor : torch.Tensor, bitwidth=4, lookuptable = None):
-    pass 
+    if lookuptable is None: 
+        n_clusters = 0 
+        kmeans = KMeans(n_clusters=n_clusters, mode='euclidean', verbose=0)
+        labels = kmeans.fit_predict(fp32_tensor.view(-1,-1)).to(torch.long)
+        centroids = kmeans.centroids.to(torch.float).view(-1)
+        lookuptable = LookupTable(centroids, labels)    
+
+    
+    #need to do: decode the quantized tensor 
+
